c3c9785bfc873b8242148aec7e30f48af8bcc38a
Martin Sandoval <martinsandoval94@hotmail.com>
2026-04-27 15:39:00 -0300

    remove ImageList component

--- app/components/common/ImageList/ImageList.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-
-import HouseCardGrid from "@/app/components/common/HouseCardGrid/HouseCardGrid";
-import { useHouses } from "@/app/hooks/useHouses";
-
-export default function ImageList() {}
